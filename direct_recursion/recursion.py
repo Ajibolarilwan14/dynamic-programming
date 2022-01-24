882cda4d59c8c206b423d485f076f4324b2d3528
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-24 10:24:04 +0100

    indirect recursion

--- direct_recursion/recursion.py
+++ direct_recursion/recursion.py
@@ -1,3 +1,4 @@
+#Direct Recursion
 def direct_recursion(str, num):
     if num > 0:
         print(str, "Calling the recursive func with num = ", num)
@@ -5,5 +6,23 @@ def direct_recursion(str, num):
     
 def main():
     direct_recursion("main", 7)
+
+# main()
+
+#Indirect Recursion
+def func1(str, num):
+    #Base Case
+    if num > 0:
+        print(str, "Called func1 with num =", num)
+        func2("func1", num-1)
+
+def func2(str, num):
+    #Base Case
+    if num > 0:
+        print(str, "Called func2 with num =", num)
+        func1("func2", num-1)
+    
+def main():
+    func1("main", 7)
 
 main()
